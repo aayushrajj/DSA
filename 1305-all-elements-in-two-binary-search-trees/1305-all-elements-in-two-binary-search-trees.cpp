3666798ddb701a0553a9d2ddb1969b70563a9e33
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-01-27 00:54:31 +0530

    Time: 358 ms (5.01%), Space: 84.5 MB (67.43%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -12,19 +12,32 @@
 class Solution {
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> list;
-        inOrder(root1,list);
-        inOrder(root2,list);
+        vector<int> ans;
+        stack<TreeNode*> s1 , s2;
         
-        sort(list.begin(),list.end());
-        return list;
-    }
-    
-    void inOrder(TreeNode* root , vector<int> &list){
-        if(root==NULL)
-            return;
-        inOrder(root->left,list);
-        list.push_back(root->val);
-        inOrder(root->right,list);
+        while(root1 || root2 || !s1.empty() || !s2.empty()){
+            while(root1){
+                s1.push(root1);
+                root1 = root1->left;
+            }
+            while(root2){
+                s2.push(root2);
+                root2 = root2->left;
+            }
+            
+            if(s2.empty() || (!s1.empty() && s1.top()->val<=s2.top()->val)){
+                root1 = s1.top();
+                s1.pop();
+                ans.push_back(root1->val);
+                root1 = root1->right;
+            }
+            else{
+                root2 = s2.top();
+                s2.pop();
+                ans.push_back(root2->val);
+                root2 = root2->right;
+            }    
+        }
+        return ans;
     }
 };
