43f5863bdd44db61c043128c104ddd3ec0b178bd
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-02-12 14:35:24 +0530

    Time: 4 ms (97.29%), Space: 7.3 MB (82.85%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -7,10 +7,12 @@ public:
         
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++){
-                if(i==0)
-                    dp[i][j] = j;
-                if(j==0)
-                    dp[i][j] = i;
+                if(i==0 && j==0)
+                    dp[i][j] = 0;
+                else if(i==0)
+                    dp[i][j] = 1 + dp[i][j-1];
+                else if(j==0)
+                    dp[i][j] = 1 + dp[i-1][j];
             }
         }
         
