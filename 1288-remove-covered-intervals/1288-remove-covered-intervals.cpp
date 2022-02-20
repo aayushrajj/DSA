c68592eef76acfbe3d33bf58d63e27c56e95cb2f
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-02-21 01:26:35 +0530

    Time: 42 ms (36.23%), Space: 12 MB (21.69%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,27 +1,16 @@
-bool mysort(vector<int> &a , vector<int> &b){
-    return a[1]<b[1];
-}
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(),intervals.end(),mysort);
-        vector<bool> map(n,false);
+        sort(intervals.begin(),intervals.end());
         int count=0;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(map[j]==false && intervals[i][0]<=intervals[j][0]){
-                    count++;
-                    map[j]=true;
-                }
-                else if(map[j]==false && intervals[i][1]==intervals[j][1]){
-                    if(intervals[i][0]>intervals[j][0]){
-                        count++;
-                        map[i]=true;
-                    }
-                }
+        int left = -1 , right = -1;
+        for(auto v : intervals){
+            if(v[0]>left && v[1]>right){
+                left = v[0];
+                count++;
             }
+            right = max(right,v[1]);
         }
-        return n-count;
+        return count;
     }
 };
