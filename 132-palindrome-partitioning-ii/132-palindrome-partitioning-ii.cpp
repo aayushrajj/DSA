4055e36e73d243a632a6ad214a1064c0f2e2d185
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-02-23 20:05:59 +0530

    Time: 66 ms (67.03%), Space: 22.4 MB (48.92%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int dp[2001][2001];
+    
     int minCut(string s){
-        memset(dp,-1,sizeof(dp));
-        return solve(s,0,s.length()-1);
-    }
-    
-    bool isPalindrome(string &s,int start,int end){
-        while(end>start){
-            if(s[start]!=s[end])
-                return false;
-            start++; end--;
+        int n = s.length();
+        int dp[n][n];
+        memset(dp,0,sizeof(dp));
+        for(int g=0;g<n;g++){
+            for(int i=0,j=g;j<n;i++,j++){
+                if(g==0)
+                    dp[i][j]=1;
+                else if(g==1 && s[i]==s[j])
+                    dp[i][j]=1;
+                else if(s[i]==s[j] && dp[i+1][j-1]==1)
+                    dp[i][j]=1;
+            }
         }
-        return true;
-    }
-    
-    int solve(string& s,int i,int j){
-        if(i>=j)
-            return 0;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if((isPalindrome(s,i,j)))
-            return 0;
-        int minans = INT_MAX;
-        for(int k=i;k<=j-1;k++){
-            if(isPalindrome(s,i,k)){
-                int temp = solve(s,i,k) + solve(s,k+1,j) + 1;
-                minans = min(minans,temp);
-            }    
+        
+        vector<int> store(n);
+        store[0]=0;
+        for(int j=1;j<n;j++){
+            if(dp[0][j]==1)
+                store[j]=0;
+            else{
+                int minans = INT_MAX;
+                for(int i=j;i>=1;i--){
+                    if(dp[i][j]==1){
+                        if(store[i-1]<minans)
+                            minans = store[i-1];
+                    }
+                }
+                store[j] = minans+1;
+            }
         }
-        return dp[i][j] = minans;
+        return store[n-1];
     }
 };
