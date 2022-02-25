6413c9c9d9669f4a7a0abf79275562672a28b725
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-02-25 14:16:43 +0530

    Time: 346 ms (50.98%), Space: 24.2 MB (38.46%) - LeetHub

---
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int pal[2001][2001];
+    bool checkPartitioning(string s) {
+        int n = s.length();
+        memset(pal,0,sizeof(pal));
+        for(int g=0;g<n;g++){
+            for(int i=0,j=g;j<n;i++,j++){
+                if(g==0)
+                    pal[i][j]=1;
+                else if(g==1 && s[i]==s[j])
+                    pal[i][j]=1;
+                else{
+                    if(s[i]==s[j] && pal[i+1][j-1])
+                        pal[i][j]=1;
+                }
+            }
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(pal[0][i-1] && pal[i][j-1] && pal[j][n-1])
+                    return true;
+            }
+        }
+        return false;
+    }
+};
