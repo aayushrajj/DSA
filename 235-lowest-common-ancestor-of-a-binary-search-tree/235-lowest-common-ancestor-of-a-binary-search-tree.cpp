b3564fca54d99daca1ae54b5f29f35b4d0533d6f
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-08 15:51:42 +0530

    Time: 45 ms (41.24%), Space: 23.3 MB (64.90%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,30 +11,15 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
-        vector<TreeNode*> first;
-        vector<TreeNode*> second;
-        helper(root,p,first);
-        helper(root,q,second);
-        for(auto i=0;i<first.size();i++){
-            for(auto j=0;j<second.size();j++){
-                if(first[i]==second[j])
-                    return first[i];
-            }
+        TreeNode* curr = root;
+        while(curr){
+            if(p->val < curr->val && q->val < curr->val)
+                curr = curr->left;
+            else if(p->val > curr->val && q->val > curr->val)
+                curr = curr->right;
+            else
+                break;
         }
-        return NULL;
-    }
-    
-    void helper(TreeNode* root,TreeNode* p,vector<TreeNode*> &first){
-        if(root==NULL) return;
-        if(root==p){
-            first.push_back(root);
-            return;
-        }
-        helper(root->left,p,first);
-        if(first.size()==0){
-            helper(root->right,p,first);
-        }
-        if(first.size()!=0)
-            first.push_back(root);
+        return curr;
     }
 };
