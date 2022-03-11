b6b9dba4dc60361690214a38e582e7e14a376805
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-11 20:47:48 +0530

    Time: 3 ms (73.54%), Space: 6.4 MB (55.54%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -3,71 +3,20 @@ public:
     string addBinary(string a, string b) {
         string res = "";
         int i=a.length()-1 , j=b.length()-1;
-        int sum=0,carry=0;
-        while(i>=0 && j>=0){
-            sum = a[i]-'0' + b[j]-'0';
-            sum += carry;
-            if(sum==0){
-                carry=0;
-                res.insert(0,"0"); 
-            }
-            else if(sum==1){
-                carry = 0;
-                res.insert(0,"1");
-            }
-            else if(sum==2){
-                carry= 1;
-                res.insert(0,"0");
-            }
-            else{
-                carry = 1;
-                res.insert(0,"1");
-            }
+        int carry=0;
+        while(i>=0 || j>=0){
+            int sum = carry;
+            if(i>=0)
+                sum += a[i]-'0';
+            if(j>=0)
+                sum += b[j]-'0';
+            carry = sum > 1 ? 1 : 0;
+            res += to_string(sum%2);
             i--; j--;
         }
-        while(i>=0){
-            sum = carry + a[i]-'0';
-            if(sum==0){
-                carry=0;
-                res.insert(0,"0"); 
-            }
-            else if(sum==1){
-                carry = 0;
-                res.insert(0,"1");
-            }
-            else if(sum==2){
-                carry= 1;
-                res.insert(0,"0");
-            }
-            else{
-                carry = 1;
-                res.insert(0,"0");
-            }
-            i--;
-        }
-        while(j>=0){
-            sum = carry + b[j]-'0';
-            if(sum==0){
-                carry=0;
-                res.insert(0,"0"); 
-            }
-            else if(sum==1){
-                carry = 0;
-                res.insert(0,"1");
-            }
-            else if(sum==2){
-                carry= 1;
-                res.insert(0,"0");
-            }
-            else{
-                carry = 1;
-                res.insert(0,"0");
-            }
-            j--;
-        }
-        if(carry>0){
-            res.insert(0,"1");
-        }
+        if(carry)
+            res += to_string(carry);
+        reverse(res.begin(),res.end());
         return res;
     }
 };
