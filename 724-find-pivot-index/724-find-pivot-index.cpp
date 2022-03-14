9063c004eebf77ff7c380c716b5f153207462462
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-14 20:15:34 +0530

    Time: 40 ms (37.31%), Space: 31.1 MB (71.60%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n,0);
-        prefix[0] = nums[0];
-        vector<int> suffix(n,0);
-        suffix[n-1] = nums[n-1];
-        for(int i=1;i<n;i++){
-            prefix[i] = prefix[i-1]+nums[i];
+        int total = 0;
+        for(int num : nums){
+            total += num;
         }
-        for(int i=n-2;i>=0;i--){
-            suffix[i] = suffix[i+1]+nums[i];
-        }
+        int prefixsum = 0;
         for(int i=0;i<n;i++){
-            if(i==0 && suffix[i+1]==0)
+            if(prefixsum*2==total-nums[i])
                 return i;
-            else if(i==n-1 && prefix[i-1]==0)
-                return i;
-            else if(i>0 && i<n-1 && prefix[i-1]==suffix[i+1])
-                return i;
+            prefixsum += nums[i];
         }
         return -1;
     }
