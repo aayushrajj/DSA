d0bd7b36bc0ff6be1b043347051981c99a3560a2
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-17 19:07:01 +0530

    Time: 15 ms (88.88%), Space: 13.6 MB (68.97%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -5,8 +5,8 @@ public:
         sort(nums.begin(),nums.end());
         if(n==0) return 0;
         int diff=INT_MAX;
-        for(int i=k-1;i<n;i++){
-            diff = min(diff,nums[i]-nums[i-k+1]);
+        for(int i=0;i<n-k+1;i++){
+            diff = min(diff,abs(nums[i+k-1]-nums[i]));
         }
         return diff==INT_MAX ? 0 : diff;
     }
