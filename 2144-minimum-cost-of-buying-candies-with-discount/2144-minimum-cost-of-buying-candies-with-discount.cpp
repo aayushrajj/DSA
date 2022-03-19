bc08702b59c2e69f9e30d30e65f80883a55623aa
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-19 19:33:25 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (79.74%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,13 +4,11 @@ public:
         int n = cost.size();
         sort(cost.begin(),cost.end(),greater<int>());
         int res=0;
-        int i = 0;
-        while(i<n){
+        for(int i=0;i<n;i=i+3){
             if(i==n-1)
                 res += cost[i];
             else
                 res += cost[i]+cost[i+1];
-            i = i+3;
         }
         return res;
     }
