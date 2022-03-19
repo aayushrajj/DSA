9079c6895a162a4d990c6153270dcd8347c1d4ba
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-20 00:02:18 +0530

    Time: 587 ms (71.99%), Space: 286.5 MB (81.80%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.cpp
@@ -3,46 +3,21 @@ public:
     vector<int> maxScoreIndices(vector<int>& nums) {
         int n = nums.size();
         vector<int> res;
-        vector<int> score(n+1,0);
-        vector<int> prefixZero(n,0);
-        vector<int> suffixOne(n,0);
-        for(int i=0;i<n;i++){
-            if(i==0){
-                if(nums[i]==0)
-                    prefixZero[i] = 1;
-            }
-            else if(nums[i]==0)
-                prefixZero[i] = prefixZero[i-1] + 1;
-            else
-                prefixZero[i] = prefixZero[i-1];
-        }
-        for(int i=n-1;i>=0;i--){
-            if(i==n-1){
-                if(nums[i]==1)
-                    suffixOne[i] = 1;
-            }
-            else if(nums[i]==1)
-                suffixOne[i] = suffixOne[i+1] + 1;
-            else
-                suffixOne[i] = suffixOne[i+1];
-        }
+        int one = accumulate(begin(nums),end(nums),0);
+        int zero = 0;
+        int maxscore = INT_MIN;
         for(int i=0;i<=n;i++){
-            if(i==0)
-                score[i] = suffixOne[i];
-            else if(i==n)
-                score[i] = prefixZero[i-1];
-            else
-                score[i] = prefixZero[i-1] + suffixOne[i];
-        }
-        int prev = INT_MIN;
-        for(int i=0;i<=n;i++){
-            if(score[i]>prev){
+            if(zero + one > maxscore){
+                maxscore = zero + one;
                 res.clear();
                 res.push_back(i);
-                prev = score[i];
             }
-            else if(score[i]==prev)
+            else if(zero + one == maxscore)
                 res.push_back(i);
+            if(i<n){
+                if(nums[i]==1) one--;
+                if(nums[i]==0) zero++;
+            }
         }
         return res;
     }
