956643d95165d121be279ee91b9f4599379612ee
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-23 00:16:30 +0530

    Time: 388 ms (86.52%), Space: 167.3 MB (65.64%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
         int n = nums.size();
-        // sort(begin(nums),end(nums));
         vector<int> res;
         unordered_map<int,int> map;
         for(int i=0;i<n;i++){
