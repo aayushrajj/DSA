36601ce23ecab224f5bc38354f609e9402293a35
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-23 00:01:40 +0530

    Time: 368 ms (34.15%), Space: 99 MB (86.90%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -3,8 +3,8 @@ public:
     int maxFrequency(vector<int>& nums, int k) {
         int n = nums.size();
         sort(begin(nums),end(nums));
-        long long left=0,right=0,total=0;
-        long long maxlen=0;
+        long long left=0,right=0;
+        long long total=0,maxlen=0;
         while(right<n){
             total += nums[right];
             if(nums[right]*(right-left+1)-total <= k){
@@ -14,7 +14,7 @@ public:
                 total -= nums[left];
                 left++;
             }
-            right++;   
+            right++;
         }
         return maxlen;
     }
