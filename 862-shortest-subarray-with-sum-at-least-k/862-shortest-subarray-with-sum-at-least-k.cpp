666c84e9fd3ffaa34b00d78d181b7ebee40922c2
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-24 20:21:02 +0530

    Time: 240 ms (86.61%), Space: 110.3 MB (53.64%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.cpp
@@ -8,12 +8,12 @@ public:
         int res = INT_MAX;
         for(int i=0;i<n;i++){
             if(i>0)
-                sum[i] += sum[i-1]+nums[i];
+                sum[i] = sum[i-1]+nums[i];
             if(sum[i]>=k)
                 res = min(res,i+1);
-            while(q.size()>0 && sum[i]-sum[q.front()]>=k)
+            while(q.size()>0 && sum[i]-sum[q.front()] >= k)
                 res = min(res,i-q.front()) , q.pop_front();
-            while(q.size()>0 && sum[i] <= sum[q.back()])
+            while(q.size()>0 && sum[i]<=sum[q.back()])
                 q.pop_back();
             q.push_back(i);
         }
