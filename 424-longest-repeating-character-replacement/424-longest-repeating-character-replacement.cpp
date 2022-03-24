797b9a589b69a9e21006c482ff1ec200891e294e
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-24 16:43:13 +0530

    Time: 7 ms (88.72%), Space: 7.1 MB (51.46%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -3,11 +3,11 @@ public:
     int characterReplacement(string s, int k) {
         int n = s.length();
         vector<int> count(26,0);
-        int left=0,maxlen=0,maxcount=0;
+        int left=0,maxlen=0,maxfreq=0;
         for(int i=0;i<n;i++){
             count[s[i]-'A']++;
-            maxcount = max(maxcount,count[s[i]-'A']);
-            if(i-left+1 - maxcount > k){
+            maxfreq = max(maxfreq,count[s[i]-'A']);
+            if(i-left+1 - maxfreq > k){
                 count[s[left]-'A']--;
                 left++;
             }
