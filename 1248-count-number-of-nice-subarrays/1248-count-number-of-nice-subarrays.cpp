cf210462403533061c3392ccd8509c714a29764b
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-25 22:55:33 +0530

    Time: 271 ms (32.07%), Space: 90.3 MB (27.24%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        // total number of substrings with at most k diff integers -
-        // total number of substrings with at most k-1 diff integers
-        return helper(nums,k) - helper(nums,k-1);
-    }
-    
-    int helper(vector<int> &nums , int k){
         int n = nums.size();
-        int left=0,res=0,count=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]&1)
-                count++;
-            while(count>k){
-                if(nums[left]&1)
-                    count--;
-                left++;
-            }
-            res += i-left+1;
+        for(auto &num : nums){
+            num = num%2;
         }
-        return res;
+        unordered_map<int,int> map;
+        int sum =0,count=0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(sum==k)
+                count++;
+            if(map.find(sum-k)!=map.end())
+                count += map[sum-k];
+            map[sum]++;
+        }
+        return count;
     }
 };
