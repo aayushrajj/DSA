24a7c6f50e6c55a07aa67961f513231478f9bb40
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-28 13:37:24 +0530

    Time: 6 ms (72.33%), Space: 9.9 MB (91.63%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    sort(nums1.begin(),nums1.end());
-    sort(nums2.begin(),nums2.end());
-    int i=0, j=0, cur=0;
-    vector<int> ans;
-    while(i<nums1.size() && j<nums2.size())
-    {
-        if(nums1[i]==nums2[j])
-        {
-            ans.push_back(nums1[i]);
-            cur=nums1[i];
-            
-            while(i<nums1.size() && nums1[i]==cur)
-                i++;
-            
-            while(j<nums2.size() && nums2[j]==cur)
-                j++;
-            
-            continue;
-            
+        auto size1 = nums1.size();
+        auto size2 = nums2.size();
+        
+        if(size1 == 0 || size2 == 0) return vector<int>();
+        
+        // assume sorted
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        
+        // we will use nums1 as a space for putting intersected elements
+        // P is an index counter for it
+        auto p = 0; 
+
+        auto p1 = 0; // counter for iterating nums1
+        auto p2 = 0; // counter for iterating nums2
+        
+        while(true) {
+            auto a = nums1[p1];
+            auto b = nums2[p2];
+
+            if(a == b && (p == 0 || a != nums1[p-1])) {
+                nums1[p] = a; // we are setting intersected elements to the beginning of nums1
+                p++;
+            }
+            if(a < b) p1++;
+            else p2++;
+            if(p1 == size1 || p2 == size2) break;
         }
-        else if(nums1[i] < nums2[j])
-            i++;
-        else
-            j++;
-    }
-    return ans;
+        
+        nums1.erase(nums1.begin() + p, nums1.end()); // and we are erasing all other elements from nums1
+        return nums1;
     }
 };
