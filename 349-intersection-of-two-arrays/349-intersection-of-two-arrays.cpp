ca76f26f7e6f89f9a1f834d018fd7f5f369b2008
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-28 13:24:13 +0530

    Time: 11 ms (37.80%), Space: 10 MB (91.63%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        unordered_map<int,int> map1;
-        for(int v : nums1)
-            map1[v]++;
-        for(int i=0;i<nums2.size();i++){
-            if(map1[nums2[i]]>0){
-                ans.push_back(nums2[i]);
-                map1[nums2[i]] = 0;
-            }
+    sort(nums1.begin(),nums1.end());
+    sort(nums2.begin(),nums2.end());
+    int i=0, j=0, cur=0;
+    vector<int> ans;
+    while(i<nums1.size() && j<nums2.size())
+    {
+        if(nums1[i]==nums2[j])
+        {
+            ans.push_back(nums1[i]);
+            cur=nums1[i];
+            
+            while(i<nums1.size() && nums1[i]==cur)
+                i++;
+            
+            while(j<nums2.size() && nums2[j]==cur)
+                j++;
+            
+            continue;
+            
         }
-        return ans;
+        else if(nums1[i] < nums2[j])
+            i++;
+        else
+            j++;
+    }
+    return ans;
     }
 };
