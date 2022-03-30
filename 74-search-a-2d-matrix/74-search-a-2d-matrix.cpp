605cff01e94f9faf1c8b055df4bd19f89023c656
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-03-30 23:50:37 +0530

    Time: 4 ms (77.73%), Space: 9.6 MB (52.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,14 +1,54 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(target==matrix[i][j])
-                    return true;
+    if (matrix.size() == 0 || matrix[0].size() == 0)
+    {
+        return false;
+    }
+
+    int low;
+    int high;
+
+    // first search in first column, use binary search
+    for (low = 0, high = matrix.size() - 1; low <= high;)
+    {
+        int middle = (low + high) / 2;
+        if (matrix[middle][0] < target)
+        {
+            low = middle + 1;
+        }
+        else if (matrix[middle][0] > target)
+        {
+            high = middle - 1;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    // when above loop ends, search in row[high]
+    int row = high;
+    if (row >= 0)
+    {
+        for (low = 0, high = matrix[row].size() - 1; low <= high;)
+        {
+            int middle = (low + high) / 2;
+            if (matrix[row][middle] < target)
+            {
+                low = middle + 1;
+            }
+            else if (matrix[row][middle] > target)
+            {
+                high = middle - 1;
+            }
+            else
+            {
+                return true;
             }
         }
-        return false;
+    }
+
+    return false;
     }
 };
