7982fd6fe877d70902da297ea37211c4e88d83a7
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-04-01 11:54:06 +0530

    Time: 4 ms (69.43%), Space: 7.4 MB (89.48%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> allsubsets;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        int n = nums.size();
         sort(begin(nums),end(nums));
-        vector<int> empty;
-        generate(empty,0,nums);
-        return allsubsets;
-    }
-    
-    void generate(vector<int> &subset,int i,vector<int> &nums){
-        if(i==nums.size()){
-            allsubsets.push_back(subset);
-            return;
+        vector<vector<int>> res{{}};
+        int size =0 , startidx =0;
+        for(int i=0;i<n;i++){
+            startidx = i>=1 && nums[i]==nums[i-1] ? size : 0;
+            size = res.size();
+            for(int j=startidx;j<size;j++){
+                res.push_back(res[j]);
+                res.back().push_back(nums[i]);
+            }
         }
-        // include ith element
-        subset.push_back(nums[i]);
-        generate(subset,i+1,nums);
-        // backtrack
-        subset.pop_back();
-        //dont choose the current ith element [hence go to the next different number]
-        int nextidx = upper_bound(nums.begin(),nums.end(),nums[i])-nums.begin();
-        generate(subset,nextidx,nums);
+        return res;
+
     }
 };
