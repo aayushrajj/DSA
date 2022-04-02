ba0874f5b0816b86fd1c124e4f770874fac786f4
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-04-02 10:46:31 +0530

    Time: 60 ms (80.88%), Space: 23.9 MB (39.29%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int n = s.length();
+        int left=0,right=n-1;
+        int count=0;
+        while(left<right){
+            if(s[left]!=s[right]){
+                return (checkPal(s,left,right-1) || checkPal(s,left+1,right));
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    
+    bool checkPal(string s , int i , int r){
+        while(i<r){
+            if(s[i]!=s[r])
+                return false;
+            i++;
+            r--;
+        }
+        return true;
+    }
+};
