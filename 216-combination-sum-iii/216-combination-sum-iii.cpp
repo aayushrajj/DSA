452d7e7da8ef86b7bb274ffbf3e1bd31de436f34
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-04-04 23:40:09 +0530

    Time: 3 ms (44.00%), Space: 6.9 MB (15.68%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    int arr[9] = {1,2,3,4,5,6,7,8,9};
     vector<vector<int>> res;
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> currVec;
-        backtrack(n,k,currVec,0);
+        backtrack(n,k,currVec,1);
         return res;
     }
     
     void backtrack(int n,int k,vector<int> currVec,int currIdx){
-        if(currVec.size()==k && n==0){
-            res.push_back(currVec);
+        if(currVec.size()==k){
+            if(n==0)
+                res.push_back(currVec);
             return;
         }
-        if(currVec.size()>=k)
-            return;
-        for(int i=currIdx;i<9;i++){
-            if(arr[i]>n)
-                break;
-            currVec.push_back(arr[i]);
-            backtrack(n-arr[i],k,currVec,i+1);
+        for(int i=currIdx;i<=9;i++){
+            currVec.push_back(i);
+            backtrack(n-i,k,currVec,i+1);
             currVec.pop_back();
         }     
     }
