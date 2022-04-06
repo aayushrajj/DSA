e0fd46eb2072483a3cfd44223ed6b51c7435cb86
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-04-07 00:27:06 +0530

    Time: 20 ms (44.92%), Space: 12.3 MB (46.10%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.cpp
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -1,40 +1,15 @@
 class Solution {
 public:
     string longestNiceSubstring(string s) {
-        int n = s.length();
-        int maxlength=0;
-        string result;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isNice(s,i,j)){
-                    if(j-i+1>maxlength){
-                        maxlength = j-i+1;
-                        result = s.substr(i,j-i+1);
-                    }
-                }
+        if (s.size() < 2) return "";
+        unordered_set<char> ck(begin(s), end(s));
+        for (int i = 0; i < s.size(); i++) {
+            if (ck.find((char) toupper(s[i])) == end(ck) or ck.find((char) tolower(s[i])) == end(ck)) {
+                string s1 = longestNiceSubstring(s.substr(0, i));
+                string s2 = longestNiceSubstring(s.substr(i + 1));
+                return s1.size() >= s2.size() ? s1 : s2;
             }
         }
-        return result;
-    }
-    
-    bool isNice(string &s,int i,int j){
-        unordered_map<int,bool> map;
-        for(int x=i;x<=j;x++){
-            map[s[x]] = true;
-        }
-        while(i<=j){
-            // if char is UPPERCASE
-            if(s[i]<97){
-                if(map[s[i]+32]==false)
-                    return false;
-            }
-            // IF CHAR IS LOWERCASE
-            else{
-                if(map[s[i]-32]==false)
-                    return false;
-            }
-            i++;
-        }
-        return true;
+        return s;
     }
 };
