8ec42c021cd4a7e173fa8ed46571c17bebbc0b2a
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-04-08 01:37:25 +0530

    Time: 1236 ms (14.73%), Space: 9.5 MB (65.42%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
-        sort(begin(nums),end(nums),greater<int>());
         int n = nums.size();
         if(n<k) return false;
         int total = accumulate(nums.begin(),nums.end(),0);
         if(total%k!=0) return false;
         int sum = total/k;
+        sort(begin(nums),end(nums),greater<int>());
         vector<bool> visited(n);
         int currSum=0;
         return backtrack(nums,visited,currSum,sum,k,0);
