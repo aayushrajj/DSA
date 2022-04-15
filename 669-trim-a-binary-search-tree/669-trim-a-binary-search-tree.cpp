2ea9f7095095f3985a2bc7bb84ff4cce7a7d342a
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-04-15 19:21:33 +0530

    Time: 24 ms (43.03%), Space: 24 MB (7.08%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.cpp
@@ -18,13 +18,14 @@ public:
     TreeNode* helper(TreeNode* root, int low , int high){
         if(root==NULL)
             return root;
-        if(root->val > high)
-            return helper(root->left,low,high);
-        if(root->val < low)
-            return helper(root->right,low,high);
-        
-        root->left = helper(root->left,low,high);
-        root->right = helper(root->right,low,high);
+        if(root->val >= low && root->val <= high){
+            root->left = helper(root->left,low,high);
+            root->right = helper(root->right,low,high);
+        } 
+        else if(root->val > high)
+            root = helper(root->left,low,high);
+        else if(root->val < low)
+            root = helper(root->right,low,high);
         return root;
     }
 };
