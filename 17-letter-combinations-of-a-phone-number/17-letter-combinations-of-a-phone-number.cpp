f41cbedd0421ca04e234a62528ea4204172d8a86
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-05-10 01:39:42 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (35.80%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -4,20 +4,21 @@ public:
     vector<string> letterCombinations(string digits) {
         if(digits.length()==0)
             return res;
-        string map[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string> map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         string s = "";
         comb(digits,map,0,s);
         return res;
     }
     
-    void comb(string digits,string map[],int idx,string s){
+    void comb(string &digits,vector<string> &map,int idx,string &s){
         if(idx==digits.length()){
             res.push_back(s);
             return;
         }
-        string val = map[digits[idx]-'0'];
-        for(int i=0;i<val.length();i++){
-            comb(digits,map,idx+1,s+val[i]);
+        for(auto val :  map[digits[idx]-'0'] ){
+            s += val;
+            comb(digits,map,idx+1,s);
+            s.pop_back();
         }
     }
 };
