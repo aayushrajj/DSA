c852f6bbf51ff22933ffac91dc05e95e5530416e
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-05-23 01:51:56 +0530

    Time: 137 ms (24.27%), Space: 27.1 MB (76.09%) - LeetHub

---
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.length();
+        if(n&1) return false;
+        int balance = 0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')
+                balance++;
+            else if(locked[i]=='0')
+                balance++;
+            else
+                balance--;
+            
+            if(balance<0)
+                return false;
+        }
+        
+        balance = 0;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]==')')
+                balance++;
+            else if(locked[i]=='0')
+                balance++;
+            else
+                balance--;
+            
+            if(balance<0)
+                return false;
+        }
+        
+        return true;
+    }
+};
