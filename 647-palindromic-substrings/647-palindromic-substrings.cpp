cd8a3acea79867f10b05dcc1d6957345bc374be5
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-05-22 14:39:10 +0530

    Time: 12 ms (59.02%), Space: 10.6 MB (38.88%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -4,23 +4,23 @@ public:
         int n = s.length();
         int dp[n][n];
         memset(dp,0,sizeof(dp));
-        int count=0;
+        int count = 0;
         for(int g=0;g<n;g++){
             for(int i=0,j=g;j<n;i++,j++){
                 if(g==0){
-                    dp[i][j]=1;
+                    dp[i][j] = 1;
                     count++;
                 }
                 else if(g==1 && s[i]==s[j]){
-                    dp[i][j]=1;
+                    dp[i][j] = 1;
                     count++;
                 }
                 else if(s[i]==s[j] && dp[i+1][j-1]==1){
-                    dp[i][j]=1;
+                    dp[i][j] = 1;
                     count++;
                 }
             }
         }
-        return count;      
+        return count;
     }
 };
