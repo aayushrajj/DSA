2d96b3684f49e3ea1909f832ab4175466ab5ccbc
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-05-24 01:38:24 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (81.31%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -17,24 +17,3 @@ public:
         return s;
     }
 };
-
-        // vector<int> open;
-        // vector<int> close;
-        // for(int i=0;i<n;i++){
-        //     if(s[i]=='(')
-        //         open.push_back(i);
-        //     if(s[i]==')')
-        //         close.push_back(i);
-        // }
-        // if(open.size()==0 || close.size()==0)
-        //     return s;
-        // int i = open.size()-1;
-        // int j = 0;
-        // while(i >=0 && j < close.size()){
-        //     reverse(s.begin()+open[i]+1 , s.begin()+close[j]);
-        //     s[open[i]] = '*';
-        //     s[close[j]] = '*';
-        //     i--;
-        //     j++;
-        // }
-        // s.erase(remove(s.begin(),s.end(),'*'),s.end());
