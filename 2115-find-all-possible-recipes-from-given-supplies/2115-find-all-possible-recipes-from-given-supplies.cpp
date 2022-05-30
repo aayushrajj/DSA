9c07ed0b2cc864b9e8b10bfa2b0243972d607073
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-05-31 05:29:26 +0530

    Time: 1504 ms (5.28%), Space: 94 MB (95.26%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,50 +1,49 @@
 class Solution {
+
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_map<string,int> map1;
-        for(auto i : supplies)
-            map1[i] = 1;
+        unordered_map<string,int> mp;
+        for(auto i:supplies) mp[i] = 1;
+        vector<string> ans;
         int n = recipes.size();
-        vector<int> created(n,0);
-        vector<string> res;
-        int prevPossible = n;
-        while(1){
-            bool flag= false;
-            for(int i=0;i<n;i++){
-                if(created[i]==0) // check if atleast one recipe is not prepared yet
-                    flag = true;
+        vector<int> pos(n,0);
+        int prev = n;
+        
+        // O(n^4)
+        
+        while(1){ // O(100 * 10^6) = O(10^8)                                    O(100 * 10^5) = O(10^7)
+            bool f = 0;
+            for(int i = 0; i < n; i++){
+                if(!pos[i]) {
+                    f = 1;
+                }
             }
-            if(!flag)
-                break;
-            for(int i=0;i<n;i++){
-                // if curr recipe is already prepared than continue;
-                if(created[i]==1)
-                    continue;
+            if(!f) break;
+            for(int i = 0; i < n; i++){ // O(100 * 10^4) = O(10^6)                      O(100 * 10^3) = O(10^5)
+                if(pos[i]) continue;
+                f = 1;
                 
-                bool check = true;
-                for(auto &j : ingredients[i]){
-                    // if needed ingredient is not available currently
-                    if( !map1.count(j) ){
-                        check = false;
+                // len , O(len)
+                // O(1)
+                for(auto &j:ingredients[i]){ // O(100 * 10 * 10) = O(10^4)               when using &reference new O(100 * 10) = O(10 ^ 3)
+                    if(!mp[j]){         //O(10)
+                        f = 0;
                         break;
                     }
                 }
-                if(check==true){
-                    res.push_back(recipes[i]);
-                    map1[recipes[i]] = 1;
-                    created[i] = 1;
+                if(f){
+                    mp[recipes[i]] = 1;
+                    ans.push_back(recipes[i]);
+                    pos[i] = 1;
                 }
             }
-            int currPossible = 0;
-            for(int i=0;i<n;i++){
-                if(created[i]==1)
-                    currPossible++;
+            int cnt = 0;
+            for(int i = 0; i < n; i++){
+                if(!pos[i]) cnt++;
             }
-            if(prevPossible==currPossible)
-                break;
-            else
-                prevPossible = currPossible;
+            if(cnt == prev) break;
+            prev = cnt;
         }
-        return res;
+        return ans;
     }
 };
