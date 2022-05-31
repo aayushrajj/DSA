b771fe2c991d4caf6b17f76e8301abc8f488fe32
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-06-01 02:02:45 +0530

    Time: 200 ms (38.12%), Space: 58.9 MB (10.25%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
+    struct node{
+        int dest;
+        int wt;
+    };
+    struct qnode{
+        int vertex;
+        int dist;
+    };
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>> adj[n+1];
-        vector<int> signalReceivedAt(n+1,INT_MAX);
-        
-        for(auto v : times){
-            int source = v[0];
-            int dest = v[1];
-            int travelTime = v[2];
-            adj[source].push_back({travelTime,dest});
+        unordered_map< int,vector<node*> > adj;
+        for(int i=0;i<times.size();i++){
+            node *newnode = new node;
+            newnode->dest = times[i][1];
+            newnode->wt = times[i][2];
+            adj[times[i][0]].push_back(newnode);
         }
         
+        qnode *newqnode = new qnode;
+        newqnode->vertex = k;
+        newqnode->dist = 0;
+        queue<qnode*> q;
+        q.push(newqnode);
         
-        signalReceivedAt[k] = 0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,k});
         
-        while(!pq.empty()){
-            int node = pq.top().second;
-            int currNodeTime = pq.top().first;
-            pq.pop();
-            
-            for(auto x : adj[node]){
-                int time = x.first;
-                int neighbourNode = x.second;
-                if( signalReceivedAt[neighbourNode] > currNodeTime + time){
-                    signalReceivedAt[neighbourNode] = currNodeTime + time;
-                    pq.push({signalReceivedAt[neighbourNode], neighbourNode});
+        vector<int> distance(n+1,INT_MAX);
+        distance[k] = 0;
+        while(!q.empty()){
+            qnode *curr = q.front();
+            q.pop();
+            for(auto it :  adj[curr->vertex]){
+                qnode *temp = new qnode;
+                temp->vertex = it->dest;
+                temp->dist = it->wt + curr->dist;
+                if(distance[temp->vertex] > temp->dist){
+                    distance[temp->vertex] = temp->dist;
+                    q.push(temp);
                 }
             }
         }
         
-        
         int minTime = INT_MIN;
         for(int i=1;i<=n;i++){
-            minTime = max(minTime , signalReceivedAt[i]);
+            minTime = max(minTime , distance[i]);
         }
         
-        return minTime==INT_MAX ? -1 : minTime;
+        return minTime == INT_MAX ? -1 : minTime;
     }
 };
