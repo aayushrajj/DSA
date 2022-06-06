244581ea0663acaa078f2c877084ffbaf3324e1e
aayushrajj <53904167+aayushrajj@users.noreply.github.com>
2022-06-06 10:36:46 +0530

    Time: 327 ms (32.90%), Space: 66.1 MB (62.69%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-        vector<double> dist(n);
-        dist[start] = 1;
+        vector<vector<pair<int,double>>> graph(n);
         
-        for(double i=0;i<n-1;i++){
-            bool flag = false;
-            for(double j=0;j<edges.size();j++){
-                double u = edges[j][0];
-                double v = edges[j][1]; 
-                if(dist[v] < dist[u] * succProb[j]){
-                    dist[v] = dist[u] * succProb[j];
-                    flag = true;
-                }
-                if(dist[u] < dist[v] * succProb[j]){
-                    dist[u] = dist[v] * succProb[j];
-                    flag = true;
+        for(int i=0;i<edges.size();i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double p = succProb[i];
+            graph[u].push_back({v,p});
+            graph[v].push_back({u,p});
+        }
+        
+        
+        priority_queue<pair<double,int>>pq;
+        pq.push({1.0,start});
+        
+        vector<double> cost(n,0.0);
+        cost[start] = 1.0;
+        
+        while(!pq.empty()){
+            auto [prob,node] = pq.top();
+            pq.pop();
+            
+            if(node==end)
+                return prob;
+            for(auto e : graph[node]){
+                // double curr = e.first;
+                double currProb = prob * e.second;
+                if(cost[e.first] < currProb){
+                    cost[e.first] = currProb;
+                    pq.push({currProb,e.first});
                 }
             }
-            
-            if(!flag)
-                break;
         }
-          
-        return dist[end];   
+        return 0.0;
     }
 };
