861131f11b5262dcd924e2972fef4a213f31a352
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-06-09 08:59:29 +0530

    Time: 1881 ms (8.47%), Space: 377.7 MB (5.03%) - LeetHub

--- 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,36 +1,39 @@
-typedef tuple<int,int,int> ti;
 class Solution {
 public:
     int minimumObstacles(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
+        
+        vector<vector<int>> visited(m,vector<int>(n,0));
         vector<vector<int>> effort(m,vector<int>(n,INT_MAX));
         effort[0][0] = 0;
+        
         vector<vector<int>> dir = {{0,1} , {0,-1} , {1,0} , {-1,0}};
         
-        priority_queue<ti , vector<ti> , greater<ti> > pq;
-        pq.push({0,0,0}); // {cost,cellrow,cellcol}
+        deque<pair<int,int>> dq;
+        dq.push_back({0,0}); // {cellrow,cellcol}
         
-        while(!pq.empty()){
-            auto [cost,row,col] = pq.top();
-            pq.pop();
-            
-            if(row==m-1 && col==n-1)
-                return effort[m-1][n-1];
+        while(!dq.empty()){
+            auto [row,col] = dq.front();
+            dq.pop_front();
             
             for(auto d : dir){
                 int newrow = row + d[0];
                 int newcol = col + d[1];
                 if(newrow >=0 && newrow < m && newcol >=0 && newcol < n){
-                    int newcost = grid[newrow][newcol] + cost;
-                    if(effort[newrow][newcol] > newcost){
-                        effort[newrow][newcol] = newcost;
-                        pq.push({newcost,newrow,newcol});
+                    if(!visited[newrow][newcol]){
+                        effort[newrow][newcol] = effort[row][col] + grid[newrow][newcol];
+                        if(grid[newrow][newcol]==1)
+                            dq.push_back({newrow,newcol});
+                        else
+                            dq.push_front({newrow,newcol});
+                        
+                        visited[newrow][newcol] = 1;
                     }
                 }
             }
         }
         
-        return 0;
+        return effort[m-1][n-1];
     }
 };
