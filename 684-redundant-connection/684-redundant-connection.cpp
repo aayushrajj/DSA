dfef510f25313e7774e3d06eee2c57a82e6bc3e9
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-06-17 12:04:01 +0530

    Time: 64 ms (5.52%), Space: 9.6 MB (22.46%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -2,47 +2,31 @@ class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> indegree(n+1,0);
-        vector<int> graph[n+1];
-        
+        vector<vector<int>> graph(n+1);
+        vector<bool> visited(n+1);
         for(auto &e : edges){
-            indegree[e[0]]++;
-            indegree[e[1]]++;
+            
+            fill(visited.begin(),visited.end(),0);
             graph[e[0]].push_back(e[1]);
             graph[e[1]].push_back(e[0]);
+            
+            if(isCycle(graph,visited,e[0],0))
+                return e;
         }
         
-        queue<int> q;
-        for(int i=0;i<n+1;i++){
-            if(indegree[i]==1)
-                q.push(i);
+        return {};
+    }
+    
+    bool isCycle(vector<vector<int>> &graph , vector<bool> &visited , int currNode , int parent){
+        if(visited[currNode])
+            return true;
+        
+        visited[currNode] = true;
+        for(auto &child : graph[currNode]){
+            if(child!=parent && isCycle(graph,visited,child,currNode))
+                return true;
         }
         
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(auto &v : graph[curr]){
-                indegree[v]--;
-                if(indegree[v]==1)
-                    q.push(v);
-            }
-        }
-        
-        unordered_set<int> store;
-        for(int i=0;i<n+1;i++){
-            if(indegree[i]>1)
-                store.insert(i);
-        }
-        
-        vector<int> res;
-        for(auto &e : edges){
-            if(store.find(e[0]) != store.end() && store.find(e[1]) != store.end()){
-                res.clear();
-                res.push_back(e[0]);
-                res.push_back(e[1]);
-            }
-        }
-        
-        return res;
+        return false;
     }
 };
