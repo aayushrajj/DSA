3c8c977aa35f97045608ce868c433305353e109a
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-06-17 14:05:11 +0530

    Time: 8 ms (77.91%), Space: 8.9 MB (68.43%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,3 +1,4 @@
+//make a disjoint set Data Structure
 class UnionFind{
     vector<int> parents , ranks;
 public:
@@ -10,12 +11,14 @@ public:
         }
     }
     
+    //find the parent root
     int find(int x){
         if(x!=parents[x])
             parents[x] = find(parents[x]); //path compression
         return parents[x];
     }
     
+     //unite two nodes and set them to a common parent root
     bool union_set(int x,int y){
         int rootX = find(x);
         int rootY = find(y);
@@ -26,7 +29,7 @@ public:
         }
         else{
             parents[rootX] = rootY;
-            if(ranks[rootX]==ranks[rootY])
+            if(ranks[rootX]==ranks[rootY]) // union_by_rank
                 ranks[rootY]++;
         }
         return false;
@@ -37,9 +40,9 @@ class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        UnionFind uf(n);
+        UnionFind uf(n+1);
         for(auto &e : edges){
-            if( uf.union_set(e[0]-1,e[1]-1) )
+            if( uf.union_set(e[0],e[1]) )
                 return {e[0],e[1]};
         }
         return {};
