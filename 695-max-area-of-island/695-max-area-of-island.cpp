a664bb4d4b1d5e6c05345c4ac4ce929f7c3161b2
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-06-19 12:53:41 +0530

    Time: 12 ms (98.40%), Space: 23.1 MB (98.48%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -9,8 +9,7 @@ public:
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    int landCount = 0;
-                    dfs(grid,landCount,i,j);
+                    int landCount = dfs(grid,i,j);
                     count = max(count,landCount);
                 }
             }
@@ -18,25 +17,10 @@ public:
         return count;
     }
     
-    void dfs(vector<vector<int>>& grid,int &landCount,int row,int col){
-        if(row>=0 && row<m && col>=0 && col<n && grid[row][col]){
-            landCount++;
-            grid[row][col] = 0;
-            dfs(grid,landCount,row+1,col);
-            dfs(grid,landCount,row-1,col);
-            dfs(grid,landCount,row,col+1);
-            dfs(grid,landCount,row,col-1);
-        }
+    int dfs(vector<vector<int>>& grid,int row,int col){
+        if(row<0 || row>=m || col<0 || col>=n || !grid[row][col])
+            return 0;
+        grid[row][col] = 0;
+        return 1 + dfs(grid,row+1,col) + dfs(grid,row-1,col) + dfs(grid,row,col+1) + dfs(grid,row,col-1);
     }
-    
-    /*
-            grid[row][col] = 0;
-        landCount++;
-        if(row+1<m && col+1<n && grid[row][col+1]==1){
-            dfs(grid,landCount,row,col+1);
-        }
-        if(row+1<m && col+1<n && grid[row+1][col]==1){
-            dfs(grid,landCount,row+1,col);
-        }
-    */
 };
