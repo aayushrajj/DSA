437881954211a811bb495aeb79cebb969d7cd074
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-06-19 15:17:07 +0530

    Time: 26 ms (64.04%), Space: 26.8 MB (13.79%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     int m,n;
+    vector<int> offset = {0,1,0,-1,0};
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        int count =0;
-        
+        int area = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    int landCount = dfs(grid,i,j);
-                    count = max(count,landCount);
+                if(grid[i][j]){
+                    int currArea = 1;
+                    grid[i][j] = 0;
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        auto curr = q.front();
+                        q.pop();
+                        for(int k=0;k<4;k++){
+                            int newrow = curr.first + offset[k];
+                            int newcol = curr.second + offset[k+1];
+                            if (newrow >= 0 && newrow < m && newcol >= 0 && newcol < n && grid[newrow][newcol]){
+                                grid[newrow][newcol] = 0;
+                                currArea++;
+                                q.push({newrow,newcol});
+                            }
+                        }
+                    }
+                    area = max(area,currArea);
                 }
             }
         }
-        return count;
-    }
-    
-    int dfs(vector<vector<int>>& grid,int row,int col){
-        if(row<0 || row>=m || col<0 || col>=n || !grid[row][col])
-            return 0;
-        grid[row][col] = 0;
-        return 1 + dfs(grid,row+1,col) + dfs(grid,row-1,col) + dfs(grid,row,col+1) + dfs(grid,row,col-1);
+        
+        return area;
+        
     }
 };
