9bf9ee6df755a43eb79375d9bf9a19bbc0914ca1
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-03 17:46:42 +0530

    Time: 93 ms (96.55%), Space: 29.9 MB (97.39%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.cpp
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -1,38 +1,84 @@
+class UnionFind{
+private:
+    vector<int> parent;
+    vector<int> ranks;
+public:
+    int groupCount;
+    UnionFind(int n){
+        groupCount = n;
+        parent.resize(n);
+        ranks.resize(n);
+    }
+    void intialize(int x){
+        parent[x] = x;
+    }
+    int find(int x){
+        if(x!=parent[x])
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    void unify(int x,int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX==rootY) return;
+        groupCount--;
+        if(ranks[rootX]>ranks[rootY])
+            parent[rootY] = rootX;
+        else{
+            parent[rootX] = rootY;
+            if(ranks[rootX]==ranks[rootY])
+                ranks[rootY]++;
+        }
+    }
+};
+
 class Solution {
 public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        unordered_map<string, int> m;
-        vector<int> union_find(accounts.size(), 0);
-        unordered_map<int, vector<string>> res_map;
-        for (int i = 0; i < accounts.size(); i++) {
-            union_find[i] = i;
-            for (int j = 1; j < accounts[i].size(); j++) {
-                if (m.find(accounts[i][j]) != m.end()) {
-                    int root1 = find(union_find, i);
-                    int root2 = find(union_find, m[accounts[i][j]]);
-                    union_find[root1] = root2;
+        int n = accounts.size();
+        UnionFind uf(n);
+        
+        unordered_map<string,int> mail2accountIdx;
+        for(int i=0;i<n;i++){
+            uf.intialize(i);
+            auto &curr = accounts[i];
+            for(int j=1;j<curr.size();j++){
+                auto &email = curr[j];
+                auto it = mail2accountIdx.find(email);
+                if(it==mail2accountIdx.end())
+                    mail2accountIdx[email] = i;
+                else{
+                    uf.unify(i,it->second);
                 }
-                else
-                    m[accounts[i][j]] = union_find[i];
             }
         }
-        for (auto it : m) {
-            int ind = find(union_find, it.second);
-            res_map[ind].push_back(it.first);
+        
+        int mergedAccountsCount = uf.groupCount;
+        vector<vector<string>> mergedAccounts(mergedAccountsCount);
+        unordered_map<int,int> groupIdx2Idx;
+        int nextIdx =0;
+        
+        for(auto &pair : mail2accountIdx){
+            auto email = pair.first;
+            auto groupId = uf.find(pair.second);
+            auto it = groupIdx2Idx.find(groupId);
+            if(it==groupIdx2Idx.end()){
+                groupIdx2Idx[groupId] = nextIdx;
+                auto owner = accounts[groupId][0];
+                mergedAccounts[nextIdx].push_back(owner);
+                mergedAccounts[nextIdx].push_back(email);
+                nextIdx++;
+            }
+            else{
+                mergedAccounts[it->second].push_back(email);
+            }
         }
-        vector<vector<string>> res;
-        for (auto it : res_map) {
-            vector<string> email = it.second;
-            sort(email.begin(), email.end());
-            email.insert(email.begin(), accounts[it.first][0]);
-            res.push_back(email);
+        
+        for(int i=0;i<mergedAccountsCount;i++){
+            auto &emails = mergedAccounts[i];
+            sort(emails.begin()+1,emails.end());
         }
-        return res;
-    }
-    
-    int find(vector<int> &union_find, int ind) {
-        while(union_find[ind] != ind)
-            ind = union_find[ind];
-        return ind;
+        
+        return mergedAccounts;
     }
 };
