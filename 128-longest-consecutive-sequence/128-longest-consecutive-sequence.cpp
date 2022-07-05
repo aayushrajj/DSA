6b2eaaaf319c385e60077d251557ced276d69b06
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-05 23:46:38 +0530

    Time: 138 ms (80.72%), Space: 33.7 MB (85.63%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -5,17 +5,16 @@ public:
         int maxlen=1,count=1;
         if(n==0) return 0;
         sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1])
-                nums.erase(nums.begin()+i),i--;
-        }
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]+1)
-                count++;
-            else
-                count = 1;
-            if(count>maxlen)
-                maxlen = count;
+        for(int i=1;i<n;i++){
+            if(nums[i]!=nums[i-1]){
+                if(nums[i]==nums[i-1]+1)
+                    count++;
+                else{
+                    count = 1;
+                }
+                if(count>maxlen)
+                    maxlen = count;
+            }  
         }
         return maxlen;
     }
