5d35f6992a6915b9fc293d37f269cb8cf77f2572
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-07 21:46:49 +0530

    Time: 7 ms (26.04%), Space: 8.3 MB (77.33%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    unordered_map<string, vector<pair<string, double>>> adjList;
+    unordered_map<string, bool> visited;
+    double queryAns;
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values,
+    vector<vector<string>>& queries) {
+        int n = equations.size();
+        int m = queries.size();
+        vector<double> result(m);
+        for(int i=0;i<n;i++){
+            adjList[equations[i][0]].push_back({equations[i][1] , values[i]});
+            adjList[equations[i][1]].push_back({equations[i][0] , 1/values[i]});
+            visited[equations[i][0]] = false;
+            visited[equations[i][1]] = false;
+        }
+        
+        for(int i=0;i<m;i++){
+            queryAns = 1;
+            bool pathFound = dfs(queries[i][0] , queries[i][1] , 1);
+            if(pathFound)
+                result[i] = queryAns;
+            else
+                result[i] = -1;
+        }
+        
+        return result;
+    }
+    
+    bool dfs(string startNode, string endNode, double runningProduct){
+        if(startNode==endNode && adjList.find(startNode)!=adjList.end()){
+            queryAns = runningProduct;
+            return true;
+        }
+        
+        bool tempAns = false;
+        visited[startNode] = true;
+        
+        for(int i = 0; i < adjList[startNode].size(); i++){
+            if( !visited[adjList[startNode][i].first] ){
+                tempAns = dfs(adjList[startNode][i].first, endNode,
+                              runningProduct*adjList[startNode][i].second);
+                if(tempAns)
+                    break;
+            }
+        }
+        
+        visited[startNode] = false;
+        return tempAns;
+    }
+};
