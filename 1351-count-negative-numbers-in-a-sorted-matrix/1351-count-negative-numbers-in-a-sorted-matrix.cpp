03cae33c61d83d0aabfd6b0a117d49806493aa30
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-15 18:32:27 +0530

    Time: 44 ms (5.35%), Space: 10.4 MB (47.11%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,21 +1,51 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int neg = 0;
-        for(int i=0,high=n-1;i<m;i++){
-            int low=0;
-            while(low<=high){
-                int mid = low + (high-low)/2;
-                if(grid[i][mid]<0)
-                    high = mid-1;
-                else
-                    low = mid+1;
-            }
-            neg += n-low;
-        }
+     
+        //we will be using binary search to serch a negative number in every row
+        //if we find the index of the neg number in ith row
+        //then the index of negative number in the i+1 row must be before or on the same index of the previous row ,since colums are sorted in non increasing order
         
-        return neg;
+        int rows=grid.size();
+        int cols=grid[0].size();
+        int lastNeg=cols-1;
+        int totalNeg=0;
+        int low=0;
+        int high;
+        for(int i=0;i<rows;i++)
+        {
+            //find negative number in that row
+            //if the last element is positive then we wont have a neg number
+            if(grid[i][cols-1]>0)
+                continue;
+            //if the first element is negative then entire row will be negative
+            if(grid[i][0]<0)
+            {
+                totalNeg+=cols;
+                continue;
+            }
+            //row contains partial positive and partial neg elements
+            high=lastNeg;
+            while(low<=high)
+            {
+                int mid=(low+high)/2;
+                if(grid[i][mid]>=0)
+                {
+                    //neg is present on the right side
+                    low=mid+1;
+                }
+                else 
+                {
+                    //neg may be present on the left of it
+                    high=mid-1;
+                }
+            }
+            //now low contains the first negative element
+            totalNeg+=cols-low;
+            lastNeg=low; 
+            low=0;
+            
+        }
+        return totalNeg;
     }
 };
