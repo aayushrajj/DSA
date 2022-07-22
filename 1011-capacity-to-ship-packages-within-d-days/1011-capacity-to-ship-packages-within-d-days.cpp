fdd2b48519c68ee0ff68d950530541a1bed3f4a4
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-23 01:16:15 +0530

    Time: 89 ms (49.40%), Space: 26.1 MB (26.55%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        
+        int low = *max_element(weights.begin(),weights.end());
+        int high = accumulate(weights.begin(),weights.end(),0);
+        
+        int best = 0;
+        while(low<=high){
+            int capacity = low + (high-low)/2;
+            int daysCount = 1;
+            int loads=0;
+            for(int i=0;i<n;i++){
+                loads += weights[i];
+                if(loads>capacity){
+                    daysCount++;
+                    loads = weights[i];
+                }
+            }
+            if(daysCount<=days){
+                best = capacity;
+                high = capacity-1;
+            } 
+            else
+                low = capacity+1;
+        }
+        
+        return best;
+    }
+};
