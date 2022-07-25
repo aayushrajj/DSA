150694e03a47b32ddfa3f384cb045670dea19cec
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-26 00:09:20 +0530

    Time: 43 ms (80.73%), Space: 12.3 MB (21.45%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -1,42 +1,57 @@
 class Solution {
 public:
-    int minimumMountainRemovals(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> left(n,1);
-        vector<int> right(n,1);
+/*
+    This question is about longest increasing sequence from both sides
+    let take example 
+        ith index
+        left[i] = x //length of increasing sequence from left side till ith index
+        right[i] = y //length of increasing sequence form right side till ith index
         
-        vector<int> dp;
-        for(int i=0;i<n;i++){
-            auto it = lower_bound(dp.begin(),dp.end(),nums[i]);
-            if(it != dp.end())
-                *it = nums[i];
-            else
-                dp.push_back(nums[i]);
-            left[i] = dp.size();
-        }
+        now x+y will be size of mountain if we suppose peak at ith index
         
-        dp.clear();
-        for(int i=n-1;i>=0;i--){
-            auto it = lower_bound(dp.begin(),dp.end(),nums[i]);
-            if(it != dp.end())
-                *it = nums[i];
-            else
-                dp.push_back(nums[i]);
-            right[i] = dp.size();
-        }
+        now how many indexes we have removed will be -> n - (x+y)
+        and we have to minimize this.
         
+    
+*/
+int minimumMountainRemovals(vector<int>& nums) {
+    vector<int> left(nums.size(), 1), right(nums.size(), 1);
+    
+    vector<int> cpy; // this will store our LIS
+    //LIS can be found in nlogn with binary search
+    
+    int n = nums.size();
+    
+    for(int i=0; i<n; i++){
+        auto it = lower_bound(begin(cpy), end(cpy), nums[i]); //chekcing if any element less than equal to ith element in our sequence with binary search 
         
-        int result = 0;
-        for (int i=0;i<n;i++)
-        {
-            if (left[i] != 1 && right[i] != 1){
-                // int x = n - (left[i]+right[i]-1);
-                // ans = min(ans, x);
-                result = max(result, left[i] + right[i]-1);
-            }
-                 
-        }
-        return n - result;
-        // return ans;
+        if(it != cpy.end()) *it = nums[i]; // if yes then we replace that element in our sequence.
+        else cpy.push_back(nums[i]); //else we push it in our sequence.
+        left[i] = cpy.size(); //and size of sequence till ith index from left side
     }
+    
+    cpy.clear();
+    
+    //same can be done from right side.
+    
+    for(int i=n-1; i>=0; i--){
+        auto it = lower_bound(begin(cpy), end(cpy), nums[i]);
+        if(it != cpy.end()) *it = nums[i];
+        else cpy.push_back(nums[i]);
+        right[i] = cpy.size();
+    }
+    
+    
+    //at last calculating ans
+    
+    int ans = n;
+    for(int i=0; i<nums.size(); i++){
+        if(left[i]-1 && right[i]-1){ //if sequence length is 1 from any side then this index can't be peak of mountain.
+            int x = n - (left[i]+right[i]-1);
+            ans = min(ans, x);
+        }
+    }
+    
+    return ans;
+}
 };
