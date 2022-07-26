2f46734c50180d645f19dcecbe3025d2adfc3dfe
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-26 22:23:48 +0530

    Time: 262 ms (70.26%), Space: 115.3 MB (44.93%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+        
+        TreeNode* lca = helper(root,startValue,destValue);
+        
+        string lca_to_start = "" , lca_to_dest = "";
+        
+        traverse(lca,startValue,lca_to_start);
+        traverse(lca,destValue,lca_to_dest);
+        
+        for(auto &c : lca_to_start) c = 'U';
+        
+        return lca_to_start + lca_to_dest;
+        
+    }
+    
+    bool traverse(TreeNode* root,int val,string &path){
+        if(root==NULL)
+            return false;
+        
+        if(root->val==val)
+            return true;
+        
+        path += 'L';
+        if(traverse(root->left,val,path)) return true;
+        path.pop_back();
+        
+        path += 'R';
+        if(traverse(root->right,val,path)) return true;
+        path.pop_back();
+        
+        return false;
+    }
+    
+    TreeNode* helper(TreeNode* root,int &p,int &q){
+        if(root==NULL)
+            return root;
+        if(root->val==p || root->val==q)
+            return root;
+        
+        TreeNode* left = helper(root->left,p,q);
+        TreeNode* right = helper(root->right,p,q);
+        
+        if(left && right)
+            return root;
+        
+        return left==NULL ? right : left;
+    }
+    
+};
