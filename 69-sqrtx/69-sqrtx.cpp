d9d898f9400c97ba46d3d4df5a5cb0c82640640b
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-07-27 20:49:00 +0530

    Time: 4 ms (55.03%), Space: 5.9 MB (24.89%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0 || x==1)
+            return x;
+        long long low=0,high=x/2;
+        while(low<=high){
+            long long mid = low + (high-low)/2;
+            if(mid*mid==x)
+                return mid;
+            else if(mid*mid>x)
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        
+        return high;       
+    }
+};
