d32f5209c4ad580a6e5ecb84e0f19f43d88afada
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-02 03:18:01 +0530

    Time: 31 ms (77.04%), Space: 13.8 MB (30.40%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -6,23 +6,25 @@ public:
         
         for(int i=0;i<n;i++){
             if(color[i]!=0)
-                continue;
+                continue;    
             color[i] = 1;
             queue<int> q;
             q.push(i);
             while(!q.empty()){
                 int node = q.front();
                 q.pop();
+               
                 for(auto &child : graph[node]){
                     if(color[child]==0){
-                        color[child] = 3-color[node];
+                        color[child] = 3 - color[node];
                         q.push(child);
                     }
-                    else if(color[node]==color[child])
+                    else if(color[child]==color[node])
                         return false;
                 }
             }
         }
+        
         return true;
     }
 };
