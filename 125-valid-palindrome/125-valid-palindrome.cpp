fa9bb65cbe8042a85e4d2bea107406359ad34f47
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-07 18:33:08 +0530

    Time: 13 ms (23.42%), Space: 7.3 MB (76.98%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string copy = "";
-        for(auto &c : s){
-            if(c>='A' && c<='Z')
-                c = c+32;
-            if(c>='a' && c<='z')
-                copy += c;
-            if(c>='0' && c<='9')
-                copy += c;
-        }
-        int len = copy.length();
-        int i=0,j=len-1;
+        int i=0,j=s.length()-1;
         while(i<j){
-            if(copy[i]!=copy[j])
+            if(!isalnum(s[i])){
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j]))
                 return false;
             i++;
             j--;
