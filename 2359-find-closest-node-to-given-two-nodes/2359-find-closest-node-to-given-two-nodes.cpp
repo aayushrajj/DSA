c17e782a0e40d209883531a78681ad3ac3e2fd61
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-08 17:51:22 +0530

    Time: 422 ms (51.62%), Space: 128.7 MB (46.76%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int n = edges.size();
-        
-        vector<int> visited(n,0);
-        
-        vector<int> dist1(n,-1);
-        vector<int> dist2(n,-1);
-        
-        int d = 0;
-        dfs(edges,visited,dist1,node1,n,d);
-        
-        d = 0;
-        fill(visited.begin(),visited.end(),0);
-        dfs(edges,visited,dist2,node2,n,d);
-        
-        int curr = INT_MAX;
-        int resIdx = -1;
-        for(int i=0;i<n;i++){
-             if(dist1[i]!=-1 && dist2[i]!=-1){
-                 int t = max(dist1[i],dist2[i]);
-                 if(t < curr){
-                     curr = t;
-                     resIdx = i;
-                 }
-             }
-        }
-        
-        return resIdx;
-    }
-    
-    void dfs(vector<int>& edges,vector<int>& visited,vector<int>& dist,int node,int n,int d){
-        visited[node] = 1;
-        dist[node] = d;
-        if(node < n && edges[node]!=-1 && edges[node] <  n && visited[edges[node]]==0)
-            dfs(edges,visited,dist,edges[node],n,d+1);
-        return;
-    }
+	int n;
+
+	vector<int> bfs(int node,vector<pair<int,int>>adj[]){
+	priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+	vector<int>distance(n,INT_MAX);
+	distance[node]=0;
+	q.push({0,node});
+
+	while(!q.empty()){
+		int dist= q.top().first;
+		int prev = q.top().second;
+		q.pop();
+		for(auto x: adj[prev]){
+			int next = x.first;
+			int nextDistance = x.second;
+			if(distance[next]>dist+nextDistance){
+				distance[next] = dist+nextDistance;
+				q.push({distance[next],next});
+			}
+		}
+	}
+	return distance; 
+}
+
+	int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+		n = edges.size();
+		vector<pair<int,int>>adj[n];
+		for(int i=0;i<n;i++) if(edges[i]>=0) adj[i].push_back({edges[i],1});
+		vector<int>dis1 = bfs(node1,adj);
+		vector<int>dis2 = bfs(node2,adj);
+		int res = -1,maxi = INT_MAX;
+		for(int i=0;i<n;i++){
+		  if(dis1[i]!=INT_MAX and dis2[i]!=INT_MAX){
+			  if(maxi>max(dis1[i],dis2[i])) {
+				  maxi = max(dis1[i],dis2[i]);
+				  res = i;
+			  }
+		  }  
+		}
+		return res;
+	}
 };
