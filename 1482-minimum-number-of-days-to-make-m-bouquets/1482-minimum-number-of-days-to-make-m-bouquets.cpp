c72da5b7e744aec8403b493eb3765e96123dc208
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-10 00:48:28 +0530

    Time: 360 ms (14.89%), Space: 63.3 MB (26.88%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
-    int minDays(vector<int>& A, int m, int k) {
-        int n = A.size(), left = 1, right = 1e9;
-        if (m * k > n) return -1;
-        while (left < right) {
-            int mid = (left + right) / 2, flow = 0, bouq = 0;
-            for (int j = 0; j < n; ++j) {
-                if (A[j] > mid) {
-                    flow = 0;
-                } else if (++flow >= k) {
-                    bouq++;
-                    flow = 0;
-                }
-            }
-            if (bouq < m) {
-                left = mid + 1;
-            } else {
-                right = mid;
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int n = bloomDay.size();
+        if (m * k > n) 
+            return -1;
+        auto p = minmax_element(bloomDay.begin(),bloomDay.end());
+        
+        int low = *p.first;
+        int high = *p.second;
+        
+        while(low<high){
+            int mid = low + (high-low)/2;
+            
+            if(canHarvest(bloomDay,mid,m,k))
+                high = mid;
+            else
+                low = mid+1;
+        }
+        
+        return low;
+    }
+    
+    bool canHarvest(vector<int> &bloomDay,int day,int m ,int k){
+        int count =0;
+        for(int i=0;i<bloomDay.size();i++){
+            if(bloomDay[i]<=day)
+                count++;
+            else
+                count=0;
+            if(count==k){
+                m--;
+                count=0;
             }
         }
-        return left;
+        // are we making all bouquets possible with current harvest date?
+        return m<=0; 
     }
 };
