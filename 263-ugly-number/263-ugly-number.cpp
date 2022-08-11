9f35c115dca95c9e4fb45574b7ad5d1ec2ffa3dd
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-11 16:34:27 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (86.79%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,24 +1,9 @@
 class Solution {
 public:
-    bool isUgly(int n) {
-        
-        for(int i=2;i<=sqrt(n);i++){
-            if(n%i==0){
-                while(n%i==0){
-                    n = n/i;
-                    if(i==2 || i==3 || i==5)
-                        continue;
-                    else
-                        return false;
-                }
-            }
-        }
-        if(n!=1){
-            if(n==2 || n==3 || n==5)
-                return true;
-            else
-                return false;
-        }
-        return true;
+    bool isUgly(int num) {
+        for (int i=2; i<6 && num; i++)
+            while (num % i == 0)
+                num /= i;
+        return num == 1;
     }
 };
