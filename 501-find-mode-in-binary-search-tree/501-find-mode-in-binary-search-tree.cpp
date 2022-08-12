42c0ce09680f79dfdb501c5db63895e1615103a3
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-13 03:54:06 +0530

    Time: 12 ms (98.63%), Space: 23.8 MB (98.01%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.cpp
@@ -13,28 +13,30 @@ class Solution {
 public:
     vector<int> res;
     vector<int> findMode(TreeNode* root) {
-        int count=0,maxfreq=0;
-        TreeNode* prev = NULL;
-        inOrderFill(root,prev,count,maxfreq);
+        int count=0,maxFreq=0;
+        TreeNode *prev = NULL;
+        inOrder(root,prev,count,maxFreq);
         return res;
     }
     
-    void inOrderFill(TreeNode* root,TreeNode* &prev,int &count,int &maxfreq){
-        if(root==NULL)
-            return;
-        inOrderFill(root->left,prev,count,maxfreq);
-        if(prev && prev->val==root->val)
+    void inOrder(TreeNode* root,TreeNode* &prev,int &count,int &maxFreq){
+        if(root==NULL) return;
+        
+        inOrder(root->left,prev,count,maxFreq);
+        
+        if(prev && root->val==prev->val)
             count++;
         else
-            count = 1;
-        if(count==maxfreq)
+            count=1;
+        if(count==maxFreq)
             res.push_back(root->val);
-        if(count>maxfreq){
+        if(count>maxFreq){
             res.clear();
             res.push_back(root->val);
-            maxfreq = count;
+            maxFreq = count;
         }
         prev = root;
-        inOrderFill(root->right,prev,count,maxfreq);
+        
+        inOrder(root->right,prev,count,maxFreq);
     }
 };
