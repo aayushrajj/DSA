e3e0bbb00f93cba83cef8d6d68ab546b3fd74e55
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-16 02:23:43 +0530

    Time: 20 ms (46.93%), Space: 6.1 MB (64.52%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int> map ={
-            { 'I' , 1} , 
-            { 'V' , 5} , 
-            { 'X' , 10} ,
-            { 'L' , 50} ,
-            { 'C' , 100} ,
-            { 'D' , 500 } ,
-            { 'M' , 1000 } };
-        
-        int sum = map[s.back()];
-        for(int i = s.length()-2;i>=0;--i){
-            if(map[s[i]] < map[s[i+1]])
-                sum -= map[s[i]];
-            else
-                sum += map[s[i]];
-        }
-        
-        return sum;
+    if (s.empty()) return 0;
+    
+    int roman[24] = {};
+    roman['I' - 'A'] = 1;
+    roman['V' - 'A'] = 5;
+    roman['X' - 'A'] = 10;
+    roman['L' - 'A'] = 50;
+    roman['C' - 'A'] = 100;
+    roman['D' - 'A'] = 500;
+    roman['M' - 'A'] = 1000;
+    
+    auto sum = 0;
+    auto right = roman[s.front() - 'A'];
+    for (int i = 1; i < s.size(); ++i) {
+        auto curr = right;
+        right = roman[s[i] - 'A'];
+        if (right > curr) 
+            sum -= curr;
+        else 
+            sum += curr;
+    }
+    
+    return sum + right;
     }
 };
