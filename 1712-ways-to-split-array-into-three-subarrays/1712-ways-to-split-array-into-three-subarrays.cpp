d92d926494969aa596a118b3247e710f6adbccf3
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-20 19:35:42 +0530

    Time: 351 ms (60.63%), Space: 88.5 MB (13.84%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -10,74 +10,44 @@ public:
             presum[i] = presum[i-1] + nums[i];
         }
         
-        // int count=0;
-//         for(int i=0;i<n-2;++i){
-//             if(sum[i]> (sum[n-1]-sum[i])/2)
-//                 continue;
-//             ll leftSum = sum[i];
-//             ll midSum=0,rightSum=0;
-            
-//             //for mid subarray
-//             int low = i+1,high=n-2;
-//             while(low<high){
-//                 int mid = low + (high-low)/2;
-//                 midSum = sum[mid]-sum[i];
-//                 rightSum = sum[n-1]-sum[mid];
-//                 if(midSum<leftSum)
-//                     low = mid+1;
-//                 else
-//                     high = mid;
-//             }
-//             int left = low;
-            
-//             // for right subarray
-//             high = n-2;
-//             while(low<high){
-//                 int mid = low + (high-low)/2;
-//                 midSum = sum[mid]-sum[i];
-//                 rightSum = sum[n-1]-sum[mid];
-//                 if(midSum>rightSum)
-//                     high= mid-1;
-//                 else
-//                     low = mid;
-//             }
-//             int right = low;
-            
-//             if(sum[right]-sum[i] <= sum[n-1]-sum[right])
-//                 count = (count + (right-left+1)%mod) % mod;
-                
-//         }
-        
-//         return count;    
-        
-        int res=0;
-        for (int j = 0; j < n - 2; ++j) {
-            if (presum[j] > (presum[n - 1] - presum[j]) / 2)
+        int count=0;
+        for(int i=0;i<n-2;++i){
+            if(presum[i]> (presum[n-1]-presum[i])/2)
                 continue;
-            int leftsum = presum[j], midsum = 0, rightsum = 0;
-            int lo = j + 1, hi = n - 2;
-            while (lo < hi) {
-                int mid = lo + ((hi - lo)>>1);
-                midsum = presum[mid] - presum[j];
-                rightsum = presum[n - 1] - presum[mid];
-                if (midsum < leftsum)
-                    lo = mid + 1;
-                else hi = mid;
+            ll leftSum = presum[i];
+            ll midSum=0,rightSum=0;
+            
+            //for mid subarray
+            int low = i+1,high=n-2;
+            while(low<high){
+                int mid = low + (high-low)/2;
+                midSum = presum[mid]-presum[i];
+                rightSum = presum[n-1]-presum[mid];
+                if(midSum<leftSum)
+                    low = mid+1;
+                else
+                    high = mid;
             }
-            int left = lo;
-            hi = n - 2;
-            while (lo < hi) {
-                int mid = hi - ((hi - lo)>>1);
-                midsum = presum[mid] - presum[j];
-                rightsum = presum[n - 1] - presum[mid];
-                if (midsum > rightsum)
-                    hi = mid - 1;
-                else lo = mid;
+            int left = low;
+            
+            // for right subarray
+            high = n-2;
+            while(low<high){
+                int mid = high - (high-low)/2;
+                midSum = presum[mid]-presum[i];
+                rightSum = presum[n-1]-presum[mid];
+                if(midSum>rightSum)
+                    high= mid-1;
+                else
+                    low = mid;
             }
-            int right = lo;
-            if (presum[right] - presum[j] <= presum[n - 1] - presum[right])
-                res = (res + (right - left + 1) % mod) % mod;
+            int right = low;
+            
+            if(presum[right]-presum[i] <= presum[n-1]-presum[right])
+                count = (count + (right-left+1)%mod) % mod;
+                
         }
-        return res;
+        
+        return count;
     }
 };
