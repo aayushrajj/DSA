9b793bb987ee131c062926f71744a15c5a14d950
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-26 14:18:01 +0530

    Time: 262 ms (64.56%), Space: 38.9 MB (49.71%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.cpp
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -1,21 +1,23 @@
 class MyCalendarTwo {
+private:
+    map<int,int> map;
 public:
-    set<pair<int,int>> books;
-    set<pair<int,int>> doubleBooks;
     MyCalendarTwo() {
         
     }
     
     bool book(int start, int end) {
-        for(auto v : doubleBooks)
-            if(v.first < end && start < v.second)
+        map[start]++;
+        map[end]--;
+        int booked=0;
+        for(auto v : map){
+            booked += v.second;
+            if(booked>=3){
+                map[start]--;
+                map[end]++;
                 return false;
-        
-        for(auto v : books)
-            if(v.first < end && start < v.second)
-                doubleBooks.insert( {max(start,v.first) , min(end,v.second) });
-        
-        books.insert({start,end});
+            }
+        }
         return true;
     }
 };
