9f8b53d02222372b2df115c777c813caeb9f7ef7
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-08-30 16:42:35 +0530

    Time: 3 ms (64.32%), Space: 6.9 MB (92.76%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -9,9 +9,7 @@ public:
         int n = matrix.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                swap(matrix[i][j] , matrix[j][i]);
             }
         }
     }
@@ -20,9 +18,7 @@ public:
         int n = matrix.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n/2;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-1-j];
-                matrix[i][n-1-j] = temp;
+                swap(matrix[i][j] , matrix[i][n-1-j]);
             }
         }
     }
