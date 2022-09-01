b00bf8caa8b6f5532e378000230301f9cc8b2080
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-09-01 23:44:33 +0530

    Time: 6 ms (20.51%), Space: 5.8 MB (96.65%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
-        while(n>0){
-            count += n%2;
-            n = n/2;
+        while(n){
+            if(n&1) count++;
+            n>>=1;
         }
         return count;
     }
