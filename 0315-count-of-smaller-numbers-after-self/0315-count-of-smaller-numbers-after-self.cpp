24a12ebd61545c8903246b35a083a583c6ea4096
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-10-19 21:10:27 +0530

    Time: 1083 ms (53.76%), Space: 108.3 MB (67.45%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,54 +1,56 @@
 class Solution {
-private:
-    void merge_sort(int start, int end, vector<pair<int, int>>& nums, vector<int>& indices, vector<pair<int, int>>& temp) {
-        if(start >= end)
-            return;
-        
-        int mid = start + (end - start) / 2;
-        
-        merge_sort(start, mid, nums, indices, temp);
-        merge_sort(mid + 1, end, nums, indices, temp);
-        
-        int left = start, right = mid + 1;
-        int idx = start;
-        int nRightLessThanLeft = 0;
-        while(left <= mid and right <= end) {
-            if(nums[left] < nums[right]) {
-                indices[nums[left].second] += nRightLessThanLeft;
-                temp[idx++] = nums[left++];
-            } else if(nums[left] > nums[right]) {
-                temp[idx++] = nums[right++];
-                nRightLessThanLeft++;
-            } else
-                left++, right++;
-        }
-        
-        while(left <= mid) {
-            indices[nums[left].second] += nRightLessThanLeft;
-            temp[idx++] = nums[left++];
-        }
-        
-        while(right <= end)
-            temp[idx++] = nums[right++];
-        
-        for(int i=start; i<=end; i++)
-            nums[i] = temp[i];
-    }
-    
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<pair<int, int>> new_nums; // {num, original_idx}
-        vector<int> indices(n, 0);
-        vector<pair<int, int>> temp;
+        vector<int> count(n);
+        vector<pair<int,int>> new_nums;
+        vector<pair<int,int>> temp;
         
-        for(int i=0; i<n; i++) {
-            new_nums.push_back({nums[i], i});
-            temp.push_back({nums[i], i});
+        for(int i=0;i<n;i++){
+            new_nums.push_back({nums[i],i});
+            temp.push_back({nums[i],i});
         }
         
-        merge_sort(0, n-1, new_nums, indices, temp);
+        mergeSort(new_nums,temp,count,0,n-1);
         
-        return indices;
+        return count;
+    }
+    
+    void mergeSort(vector<pair<int,int>> &nums,vector<pair<int,int>> &temp,vector<int> &count,int start ,int end){
+        if(start>=end) return;
+        
+        int mid = start + (end-start)/2;
+        
+        mergeSort(nums,temp,count,start,mid);
+        mergeSort(nums,temp,count,mid+1,end);
+        
+        int left = start, right = mid+1;
+        int idx = start;
+        int toRightLess = 0;
+        while(left<=mid && right <= end){
+            if(nums[left] < nums[right]){
+                count[nums[left].second] += toRightLess;
+                temp[idx++] = nums[left++];
+            }
+            else if(nums[left] > nums[right]){
+                temp[idx++] = nums[right++];
+                toRightLess++;
+            }
+            else{
+                left++ , right++;
+            }
+        }
+        
+        while(left<=mid){
+            count[nums[left].second] += toRightLess;
+            temp[idx++] = nums[left++];
+        }
+        while(right<=end){
+            temp[idx++] = nums[right++];
+        }
+        
+        for(int i=start;i<=end;i++){
+            nums[i] = temp[i];
+        }  
     }
 };
