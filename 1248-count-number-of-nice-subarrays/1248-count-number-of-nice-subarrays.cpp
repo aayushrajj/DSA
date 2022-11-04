3dcbe0e32311c6a6d1d89cecedfc1aa895885b5f
Ayush Raj <53904167+aayushrajj@users.noreply.github.com>
2022-11-04 18:24:50 +0530

    Time: 267 ms (75.21%), Space: 67.7 MB (75.11%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-//         int n = nums.size();
-//         int i=0,j=0;
-//         int total=0,ans=0,count=0;
-        
-//         while(j<n){
-//             if(nums[j]&1){
-//                 count++;
-//                 if(count>=k){
-//                     ans = 1;
-//                     while(nums[i]%2==0){
-//                         ans++;
-//                         i++;
-//                     }
-//                     total += ans;
-//                 }
-//             }
-//             else if(count>=k)
-//                 total += ans;
-//             j++;
-//         }
-//         return total;
-        
         int n = nums.size();
-        int i=0,j =0, total =0, count =0;
-        int odd=0;
-        while(i<n){
-            if(nums[i]&1){
-                odd++;
-                if(odd>=k){
-                    count=1;
-                    while(!(nums[j++]&1))
-                        count++;
-                    total += count;
+        int i=0,j=0;
+        int total=0,ans=0,count=0;
+        
+        while(j<n){
+            if(nums[j]&1){
+                count++;
+                if(count>=k){
+                    ans = 1;
+                    while(nums[i]%2==0){
+                        ans++;
+                        i++;
+                    }
+                    i++;
+                    total += ans;
                 }
             }
-            else if(odd>=k)
-                total += count;
-            i++;
+            else if(count>=k)
+                total += ans;
+            j++;
         }
-        
         return total;
+        
+//         int n = nums.size();
+//         int i=0,j =0, total =0, count =0;
+//         int odd=0;
+//         while(i<n){
+//             if(nums[i]&1){
+//                 odd++;
+//                 if(odd>=k){
+//                     count=1;
+//                     while(!(nums[j++]&1))
+//                         count++;
+//                     total += count;
+//                 }
+//             }
+//             else if(odd>=k)
+//                 total += count;
+//             i++;
+//         }
+        
+//         return total;
     }
 };
